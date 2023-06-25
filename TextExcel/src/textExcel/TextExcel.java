f9b28bbd92f8a1f7698d2ffd04b46104e12596ca
Gui <12204241+guirava@users.noreply.github.com>
2023-06-25 11:20:44 -0700

    input loop

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -5,11 +5,24 @@ import java.util.Scanner;
 
 // Update this file with your own code.
 
-public class TextExcel
-{
+public class TextExcel {
 
-	public static void main(String[] args)
-	{
-	    // Add your command loop here
+	public static void main(String[] args) {
+		// Add your command loop here
+		Scanner scanner = new Scanner(System.in);
+		String input;
+		while (true) {
+			System.out.print("TextExcel> ");
+			input = scanner.nextLine();
+			if (input.toLowerCase().equals("quit")) {
+				break;
+			}
+			System.out.println(processInput(input));
+		}
+		scanner.close();
+	}
+
+	public static String processInput(String input) {
+		return "NOT IMPLEMENTED";
 	}
 }
